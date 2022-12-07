6a377282d800242e8b061f1241a6e70fe1f86880
abdessamad-zgor <abdessamadzgor@gmail.com>
2022-12-07 16:25:18 +0100

    basic next-supa app

--- store/common/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-const persistData = createAction('PERSIST_DATA');
-const getPersistedData = createAction('GET_PERSISTED_DATA');
-const signOutUser = createAction('SIGN_OUT_USER');
-
-export { persistData, getPersistedData, signOutUser };
